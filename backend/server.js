39aded8022f45ce191980234ae42c5e7f46dfc97
Paula Gayle <paula_gayle@hotmail.com>
2023-08-24 10:58:55 +0100

    pings added to server file to keep render server alive

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
 // require('dotenv').config()
+const axios = require('axios');
+
 const dotenv = require('dotenv');
 const cors = require('cors');
 const express = require('express')
@@ -35,12 +37,30 @@ const dbConnection = async () => {
     }
 }
 
+//health check for the server pings ---------
+app.get('/api/health-check', (req, res) => {
+    res.status(200).send('Server is alive!');
+});
+//-------------------------------------------
+
 //routes
 app.use('/api/peeps', peepRoutes)
 app.use('/api/user', userRoutes)
 
 // Call the function to connect to the database
 dbConnection();
+
+//25 minute pings to keep the render server alive
+function keepServerAwake() {
+    axios.get("https://chitter-e3j5.onrender.com/api/health-check")
+        .then(response => {
+            console.log('Successfully pinged server!');
+        })
+        .catch(error => {
+            console.error('Error pinging server:', error);
+        });
+}
+setInterval(keepServerAwake, 1500000); // 25 minutes
 
 const server = app.listen(port, () => {
     const SERVERHOST = server.address().address;
